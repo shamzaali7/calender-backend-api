451296e97b80edf8a58cddf04b6c42d58e6051be
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-02 14:05:35 -0500

    date model made

---
+++ models/Date.js
@@ -0,0 +1,15 @@
+const mongoose = require ('../db/connections')
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+    title: String,
+    isCompleted: {type: Boolean, default: false}
+})
+
+const dateSchema = new Schema({
+    day: Number,
+    tasks: [taskSchema]
+})
+
+const Date = mongoose.mondel("Date", dateSchema);
+monule.exports = Date;
