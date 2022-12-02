ce76b392d19510964b2c76e07a643900387b68c4
jahime001 <jahimecamea@gmail.com>
2022-12-02 14:38:38 -0500

    seed.js fix

--- db/seed.js
+++ db/seed.js
@@ -2,9 +2,7 @@ const Date = require("../models/Date")
 const seedData = require("./seed.json")
 
 Date.deleteMany({})
-   .then(() => {
-    Date.insertMany(seedData)
-   })
+   .then(() => Date.insertMany(seedData))
    .then(console.log(Date))
    .catch(console.error)
    .finally(process.exit)
