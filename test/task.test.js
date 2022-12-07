8016101d6fdf12d3b285d01a3542b2039ab475cd
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-06 22:03:55 -0500

    first two tests

--- test/task.test.js
+++ test/task.test.js
@@ -3,22 +3,21 @@ const expect = require('chai').expect
 const supertest = require('supertest')
 const api = supertest(require('../index.js'))
 
-describe("GET /task", () => {
+describe("GET /", () => {
     it("should return a 200 response", done => {
         api
-        .get("/task")
+        .get("/api/task")
         .set("Accept", "application/json")
         .expect(200, done)
     })
+    it("should return an array", done => {
+        api
+          .get("/api/task")
+          .set("Accept", "application/json")
+          .end((error, response) => {
+            expect(response.body).to.be.an('array');
+            done()
+        })
+    })
 })
 
-
-// it("should return an array", done => {
-//     api
-//       .get("/task")
-//       .set("Accept", "application/json")
-//       .end((error, response) => {
-//         expect(response.body).to.be.an('array');
-//         done()
-//     })
-// })
